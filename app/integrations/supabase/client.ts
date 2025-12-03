37a0672845e52c1515d72bc1b7b118540c98753c
natively.dev <api@natively.dev>
2025-12-03 01:17:28 +0000

    fix: Import react-native-url-polyfill to fix window is not defined

--- app/integrations/supabase/client.ts
+++ app/integrations/supabase/client.ts
@@ -1,3 +1,5 @@
+
+import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Database } from './types';
 import { createClient } from '@supabase/supabase-js'
